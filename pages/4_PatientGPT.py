3b99cbe6f16097498bbedde5b92a0c08395c4d23
Arnold85 <arne.broedel@gmail.com>
2023-12-15 13:34:42 +0000

    switchted to gpt4

--- pages/4_PatientGPT.py
+++ pages/4_PatientGPT.py
@@ -6,7 +6,7 @@ import pandas as pd
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    st.session_state["openai_model"] = "gpt-4-1106-preview"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
