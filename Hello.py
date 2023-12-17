7eda3770381be54901e98c4afd3c973de0091c18
Arnold85 <arne.broedel@gmail.com>
2023-12-17 23:36:00 +0000

    page switch

--- Hello.py
+++ Hello.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit.logger import get_logger
+from streamlit_extras.switch_page_button import switch_page
+
 
 LOGGER = get_logger(__name__)
 
@@ -13,11 +15,10 @@ def run():
     st.write("# Herzlich Willkommen zum Anamnesetrainer")
 
 
-    st.markdown(
-        """
-        ### Um zu starten, wechsel jetzt auf die Seite [PatientGPT](/PatientGPT)
-    """
-    )
+    st.markdown("### Um zu starten, wechsel jetzt auf die Seite")
+
+    if st.button("PatientGPT"):
+        switch_page("PatientGPT")
 
 
 if __name__ == "__main__":
