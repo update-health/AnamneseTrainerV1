1d2664ad04a43a9faef0e80310b2efdf26d47861
Arnold85 <arne.broedel@gmail.com>
2023-12-18 11:38:47 +0000

    Added Password protection to Hello

--- Hello.py
+++ Hello.py
@@ -1,10 +1,10 @@
 import streamlit as st
+import hmac
 from streamlit.logger import get_logger
 from streamlit_extras.switch_page_button import switch_page
 
 
 LOGGER = get_logger(__name__)
-
 
 def run():
     st.set_page_config(
@@ -12,13 +12,41 @@ def run():
         page_icon="👩‍⚕️",
     )
 
-    st.write("# Herzlich Willkommen zum Anamnesetrainer")
+st.write("# Herzlich Willkommen zum Anamnesetrainer")
+
+#password Check
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["PASSWORD"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the passward is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
 
 
-    st.markdown("### Um zu starten, wechsel jetzt auf die Seite")
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
-    if st.button("PatientGPT"):
-        switch_page("PatientGPT")
+#actual page
+st.markdown("### Um zu starten, wechsel jetzt auf die Seite")
+
+if st.button("PatientGPT"):
+    switch_page("PatientGPT")
 
 
 if __name__ == "__main__":
