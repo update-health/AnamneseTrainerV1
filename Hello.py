1c0eb1db9dcaa56d0c8eac3739f4cd81afac7d5c
Arnold85 <arne.broedel@gmail.com>
2024-01-18 12:14:14 +0000

    Markdown Anleitung

--- Hello.py
+++ Hello.py
@@ -8,8 +8,8 @@ LOGGER = get_logger(__name__)
 
 def display_homepage():
     #Display the content of the homepage.
-    '''
-    Welchen potentiellen Einsatzzweck hat der Anamnesetrainer?
+    st.markdown("""
+    ### Welchen potentiellen Einsatzzweck hat der Anamnesetrainer?  
     In der Ausbildung von Therapeuten und Medizinern ist das Erlernen einer strukturierten Anamnese und die Entwicklung zielführender Kommunikationsfähigkeiten und Interview-Techniken von herausragender Bedeutung. 
     Dabei müssen wesentliche Merkmale von Erkrankungen erfasst und sinnvoll interpretiert werden um so differentialdiagnostische Entscheidungen auf einer fundierten Basis treffen zu können.
     Neben der Schulung grundlegender Kenntnisse, Theorien und Hilfsmittel zur Anamnese, ist das praktische Üben essentieller Teil der Ausbildung. 
@@ -20,38 +20,38 @@ def display_homepage():
     Die Abhängigkeit zu anderen Personen ist in beiden Fällen groß, was sowohl Qualität als auch Quantität der Lernerfahrungen limitieren kann.
     Chatbots, basierend auf Large Language Models, könnten als Ergänzung und vielleicht auch Alternative zu konventionnellen Methoden zum Einsatz kommen um die Anamneseführung zu trainieren, in dem diese sowohl Patienten als auch Tutoren, für das Feedback nach dem Anamnesegespräch, simulieren. 
     
-    Was soll im Rahmen der Studie evaluiert werden?
+    ### Was soll im Rahmen der Studie evaluiert werden?  
     Entscheidend für den Einsatz der Chatbots ist insbesondere, ob diese in der Lage sind die komplexen Szenarios eines Patientengesprächs ausreichend realistisch simulieren und dann in Schritt 2, auch als Tutor evaluieren zu können.
-    Der Fokus der Untersuchung liegt dabei auf der Wahrnehmung der Nutzer vom Chatbot in der Rolle eines Patienten.
-
-    Wie funktioniert die Verwendung des Anamnesetrainers?
+    Der Fokus der Untersuchung liegt dabei auf der Wahrnehmung der Nutzer vom Chatbot in der Rolle eines Patienten.  
+  
+    ### Wie funktioniert die Verwendung des Anamnesetrainers?  
     Auf der Seite "PatientGPT" kann der Nutzer aus einer Dropdownliste oben auf der Seite einen Patienten auswählen. Deren relevanten Daten sind als eine Art Skript, im Sinne eines standardisierten Patienten, im System hinterlegt.
     Es kann jederzeit ein anderer Patient aus der Liste gewählt werden. Dann wird sofort die binsherige Unterhaltung gelöscht und ein neues Gespräch startet.
     Das Gespräch wird über eine einfache Chatbox geführt, sowie viele es von Messengerdiensten (wie z.B. WhatsApp) oder anderen Chatbots (wie z.B. ChatGPT) kennen.
     Unten ist die Eingabezeile zu sehen. In diese kann per Tastatur, aber auch per Spracheingabe (abhängig vom jeweiligen System) Text eingegeben werden.
     Nach den ersten 4 Antworten im Chatfenster, erscheint unterhalb des Textes ein Button, der es dem Nutzer ermöglicht das Patientengespräch zu beenden und eine Evaluation mit dem virtuellen Tutor zu starten. 
     Dieser analysiert das Anamnesegespräch und leitet ein Feedbackgespräch dazu an.
-    Das Gespräch mit dem Patienten kann dann nicht weitergeführt, sondern nur neu gestartet werden. Jederzeit kann ganz nach oben gescrollt werden, um einen neuen Patienten zu wählen und ein neues Patientengespräch zu starten.
-
-    Wie soll ich mich verhalten?
+    Das Gespräch mit dem Patienten kann dann nicht weitergeführt, sondern nur neu gestartet werden. Jederzeit kann ganz nach oben gescrollt werden, um einen neuen Patienten zu wählen und ein neues Patientengespräch zu starten.  
+  
+    ### Wie soll ich mich verhalten?  
     Um ein insgesamt realistisches Szenario zu ermöglichen, muss nicht nur der Chatbot möglichst realistisch antworten, sondern auch der Nutzer. 
     Es kann verlockend sein den Chatbot, durch absurde oder unangemessene Sprache oder Aufforderungen an die Grenzen zu bringen. 
     Das wäre vergleichbar mit dem Rollenspiel unter Mitstudierenden in denen mindestens ein Student in seiner Rolle bewusst unsinnig agiert.
     Wir gehen davon aus, dass der Nutzer ein Interesse an der Verbesserung der eigenen Anamnesefähigkeiten hat und das Tool nutzt um ein Patientengespräch realitätsnah zu simulieren.
     Versuchen Sie daher möglichst so zu Fragen oder zu Antworten, wie Sie es bei einem echten Patienten auch tun würden. 
-    Das gleiche gilt für das anschließende Feedbackgespräch mit dem virtuellen Tutor.
-
-    Wie viele Patientengespräch soll und darf ich führen?
+    Das gleiche gilt für das anschließende Feedbackgespräch mit dem virtuellen Tutor.  
+  
+    ### Wie viele Patientengespräch soll und darf ich führen?  
     Es gibt kein vorgeschriebenes Maximum an Versuchen. Legen Sie als Teilnehmer*in der Studie jedoch zunächst Fokus auf Qualität und weniger auf Quantität der Gespräche.
     Bitte nehmen Sie sich daher ausreichend Zeit, bevor Sie ein Gespräch starten. Mindestens zwei Patientengespräche sollten umfassend und, von Ihrer Seite aus, möglichst realitätsnah geführt werden. Gerne auch mehr.
     Der Fokus der Studie liegt auf dem Chatbot in der Rolle als Patient. Das Feedbackgespräch mit dem Tutor ist daher nicht zwingend vorgeschrieben. 
-    Insbesondere wenn Sie mehrere Durchgänge von Anamnesegesprächen durchführen, können Sie das Feedbackgesräch weglassen oder abkürzen.
-
-    Wohin wende ich mich bei Fragen?
-    Wenn Sie Fragen zur Studie oder Unklarheiten bei der Verwendung des Tools haben, wenden Sie sich per Email an Arne Brödel unter brodela@cardiff.ac.uk .
-
-    ### Um zu starten, klicken Sie den Button. Sie wechseln dann zur Seite "PatientGPT".
-    '''
+    Insbesondere wenn Sie mehrere Durchgänge von Anamnesegesprächen durchführen, können Sie das Feedbackgesräch weglassen oder abkürzen.  
+  
+    ### Wohin wende ich mich bei Fragen?  
+    Wenn Sie Fragen zur Studie oder Unklarheiten bei der Verwendung des Tools haben, wenden Sie sich per Email an Arne Brödel unter brodela@cardiff.ac.uk .  
+  
+    ### Um zu starten, klicken Sie den Button. Sie wechseln dann zur Seite "PatientGPT".  
+    """)
     
     if st.button("PatientGPT"):
         switch_page("PatientGPT")
