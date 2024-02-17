e7a82f9668698799a7c116be1b6b0a0a4fca9dbb
Arnold85 <arne.broedel@gmail.com>
2024-02-17 15:42:25 +0100

    n

--- pages/7_Dateiupload.py
+++ pages/7_Dateiupload.py
@@ -26,7 +26,7 @@ st.markdown("""Das Uploadformular sieht vor, dass die Nutzer*innen einen Vorname
             Sie sollen nicht Ihren tatsächlichen Vornamen und Nachnamen eingeben, sondern stattdessen die beiden Hälften Ihres Zifferncodes eingeben.  
             """)
 if random_id_string[2]==True:
-      st.write("Nutzen Sie den unbedingt den gleichen Zifferncode, den Sie auch in den Fragebogen eingetragen haben. Dieser wird immer neu erstellt sobald die Seite verlassen wurde. Der folgende Code ist daher nur beispielhaft.")
+      st.write("***Nutzen Sie den unbedingt den gleichen Zifferncode, den Sie auch in den Fragebogen eingetragen haben. Dieser wird immer neu erstellt sobald die Seite verlassen wurde. Der folgende Code ist daher nur beispielhaft.***")
 st.write("#### "+"Sie nutzen also einfach:  First name: :red["+random_id_string[0]+"] Last name: :red["+random_id_string[1]+"]")
 st.markdown("""Über den folgenden Link gelangen Sie zu einer Seite über die Sie die Bildschirm- bzw. Sprachaufzeichnungen, aber auch z.B. gespeicherte Gesprächsprotokolle, auf meinen Cloudspeicher bei der Cardiff University hochladen können.  
             Es handelt sich um einen Microsoft Dienst. Falls Sie in Ihrem Browser mit einem Microsoft Konto angemeldet sind, wird daraus Ihr Vorname und Nachname einfach übernommen.  
