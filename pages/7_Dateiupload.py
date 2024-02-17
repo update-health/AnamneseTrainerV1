c8e5e15addeff17c3dc6442b67000827054a72d9
Arnold85 <arne.broedel@gmail.com>
2024-02-17 15:51:12 +0100

    n

--- pages/7_Dateiupload.py
+++ pages/7_Dateiupload.py
@@ -22,7 +22,7 @@ st.markdown("""Über den folgenden Link gelangen Sie zu einer Seite über die Si
             Es handelt sich um einen Microsoft Dienst. Falls Sie in Ihrem Browser mit einem Microsoft Konto angemeldet sind, wird daraus Ihr Vorname und Nachname einfach übernommen.  
             Um dies zu verhindern, kopieren Sie den Link einfach in ein neues privates Browser Fenster. Je nach Browser heißen diese "Privat", "In-private", "Inkognito" oder ähnlich.""")    
 st.image("assets/InkognitoFenster.jpg")
-st.write('oder klicken auf den unten stehenden Link mit der rechten Maustaste und wählen dann dort "InPrivate-Fenster" öffnen')
+st.write('oder klicken auf den unten stehenden Link mit der rechten Maustaste und wählen dann dort "InPrivate-Fenster" öffnen, wie in diesem Bild zu sehen:')
 st.image("assets/InkognitoFensterRechtsklick.jpg")
 if random_id_string[2]==False:
       st.write("Zur Erinnerung: Dies ist Ihr Zifferncode um den ausgefüllten Fragenbogen den hochgeladenen Dateien zuordnen zu können:")
@@ -32,8 +32,10 @@ st.markdown("""Das Uploadformular sieht vor, dass die Nutzer*innen einen Vorname
             Sie sollen nicht Ihren tatsächlichen Vornamen und Nachnamen eingeben, sondern stattdessen die beiden Hälften Ihres Zifferncodes eingeben.  
             """)
 if random_id_string[2]==True:
-      st.write("***Nutzen Sie den unbedingt den gleichen Zifferncode, den Sie auch in den Fragebogen eingetragen haben. Dieser wird immer neu erstellt sobald die Seite verlassen wurde. Der folgende Code ist daher nur beispielhaft.***")
-st.write("***"+"Sie nutzen also einfach:  First name: :red["+random_id_string[0]+"] Last name: :red["+random_id_string[1]+"]***")
+      st.markdown("""***Nutzen Sie den unbedingt den gleichen Zifferncode, den Sie auch in den Fragebogen eingetragen haben. Dieser wird immer neu erstellt sobald die Seite verlassen wurde.  
+                  Der folgende Code ist daher nur beispielhaft.***  
+                  Wäre Ihr Code also {random_id_string[0]} - {random_id_string[1]}""")
+st.write("***"+"Dann nutzen Sie einfach:  First name: :red["+random_id_string[0]+"] Last name: :red["+random_id_string[1]+"]***")
 st.markdown("""### <a href="https://cf-my.sharepoint.com/:f:/g/personal/brodela_cardiff_ac_uk/EtexNRViovRPtM1d8eXJHp8BGAnWNJfnHF_idSz1xjqDyA" target="_blank">Webseite zum Hochladen der Audiodateien</a>  
                         """,unsafe_allow_html=True)
 st.markdown("""Sollte es irgendwelche Probleme beim Hochladen der Dateien geben, melden Sie sich bitte umgehend bei mir.  
