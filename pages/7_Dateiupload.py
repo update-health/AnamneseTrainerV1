f2238e4478b8fb3e609bb5f1d5647a4c766e32a1
Arnold85 <arne.broedel@gmail.com>
2024-02-17 15:46:25 +0100

    reihenfolge

--- pages/7_Dateiupload.py
+++ pages/7_Dateiupload.py
@@ -18,6 +18,12 @@ else:
 
 st.header("Dateiupload")
 st.write("Auf dieser Seite sollen Sie alle Aufnahmen und Gesprächsprotokolle von Ihnen hochladen.")
+st.markdown("""Über den folgenden Link gelangen Sie zu einer Seite über die Sie die Bildschirm- bzw. Sprachaufzeichnungen, aber auch z.B. gespeicherte Gesprächsprotokolle, auf meinen Cloudspeicher bei der Cardiff University hochladen können.  
+            Es handelt sich um einen Microsoft Dienst. Falls Sie in Ihrem Browser mit einem Microsoft Konto angemeldet sind, wird daraus Ihr Vorname und Nachname einfach übernommen.  
+            Um dies zu verhindern, kopieren Sie den Link einfach in ein neues privates Browser Fenster. Je nach Browser heißen diese "Privat", "In-private", "Inkognito" oder ähnlich.""")    
+st.image("assets/InkognitoFenster.jpg")
+st.write('oder klicken auf den unten stehenden Link mit der rechten Maustaste und wählen dann dort "InPrivate-Fenster" öffnen')
+st.image("assets/InkognitoFensterRechtsklick.jpg")
 if random_id_string[2]==False:
       st.write("Zur Erinnerung: Dies ist Ihr Zifferncode um den ausgefüllten Fragenbogen den hochgeladenen Dateien zuordnen zu können:")
       st.write("#### "+random_id_string[0]+" - "+random_id_string[1])
@@ -28,12 +34,6 @@ st.markdown("""Das Uploadformular sieht vor, dass die Nutzer*innen einen Vorname
 if random_id_string[2]==True:
       st.write("***Nutzen Sie den unbedingt den gleichen Zifferncode, den Sie auch in den Fragebogen eingetragen haben. Dieser wird immer neu erstellt sobald die Seite verlassen wurde. Der folgende Code ist daher nur beispielhaft.***")
 st.write("***"+"Sie nutzen also einfach:  First name: :red["+random_id_string[0]+"] Last name: :red["+random_id_string[1]+"]***")
-st.markdown("""Über den folgenden Link gelangen Sie zu einer Seite über die Sie die Bildschirm- bzw. Sprachaufzeichnungen, aber auch z.B. gespeicherte Gesprächsprotokolle, auf meinen Cloudspeicher bei der Cardiff University hochladen können.  
-            Es handelt sich um einen Microsoft Dienst. Falls Sie in Ihrem Browser mit einem Microsoft Konto angemeldet sind, wird daraus Ihr Vorname und Nachname einfach übernommen.  
-            Um dies zu verhindern, kopieren Sie den Link einfach in ein neues privates Browser Fenster. Je nach Browser heißen diese "Privat", "In-private", "Inkognito" oder ähnlich.""")    
-st.image("assets/InkognitoFenster.jpg")
-st.write('oder klicken auf den unten stehenden Link mit der rechten Maustaste und wählen dann dort "InPrivate-Fenster" öffnen')
-st.image("assets/InkognitoFensterRechtsklick.jpg")
 st.markdown("""### <a href="https://cf-my.sharepoint.com/:f:/g/personal/brodela_cardiff_ac_uk/EtexNRViovRPtM1d8eXJHp8BGAnWNJfnHF_idSz1xjqDyA" target="_blank">Webseite zum Hochladen der Audiodateien</a>  
                         """,unsafe_allow_html=True)
 st.markdown("""Sollte es irgendwelche Probleme beim Hochladen der Dateien geben, melden Sie sich bitte umgehend bei mir.  
