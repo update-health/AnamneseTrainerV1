397f973f858fad5fb6ff38b055046b35b60fc2b5
Arnold85 <arne.broedel@gmail.com>
2024-02-17 03:28:56 +0100

    n

--- pages/1_Einverständnis.py
+++ pages/1_Einverständnis.py
@@ -7,8 +7,8 @@ import base64
 st.set_page_config(layout="wide")
 st.markdown("""Auf dieser Seite finden Sie umfassende Informationen über die Studie, die Voraussetzungen zur Teilnahme, Ihre Rechte und Pflichten als Teilnehmende*r, Rechte und Pflichten von mir als verantwortlichem Forschenden und der Hochschule.  
             Bitte lesen Sie die Informationen bis Ihnen alle Aspekte ausreichend klar geworden sind um an der Studie teilzunehmen.  
-            Insofern Sie sich weiterhin für die Teilnahme entscheiden, müssen Sie das Einverständnisformular ausfüllrn und digital unterschreiben.  
-            ***Nach dem Signieren müssen Sie Ihre E-Mail-Adresse angeben. Sie erhalten dann eine E-Mail in der Sie einen Link zur Bestätigung klicken müssen. Erst dann ist die Einverständniserklärung vollständig.***  
+            Insofern Sie sich weiterhin für die Teilnahme entscheiden, müssen Sie das Einverständnisformular unten auf dieser Seite ausfüllen. Sie erhalten dann erneut eine E-Mail in der Sie Ihre Identität bestätigen müssen.  
+            Dann ist Ihre Einverständniserkärung vollständig und Sie bekommen das Passwort für den Anamnesetrainer.  
             Sollte das Formular hier nicht richtig angezeigt werden, können Sie es über folgenden Link in einem neuen Tab öffnen:  
             <a href="https://drive.google.com/file/d/1TzYIduwwkN61J8eE_Qm2XFp2VtUdymhp/view">Link zu den Teilnehmenden Informationen</a>
             <iframe src="https://drive.google.com/file/d/1TzYIduwwkN61J8eE_Qm2XFp2VtUdymhp/preview" style="width: 100%; height: 1000px; max-height: 94vh " allow="autoplay"></iframe>""", unsafe_allow_html=True)
