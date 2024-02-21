bc877868eefd55f4ae8ddd1b0cc0a9d8406992c0
Arnold85 <arne.broedel@gmail.com>
2024-02-21 16:33:04 +0100

    Nachinformation

--- pages/8_Danke.py
+++ pages/8_Danke.py
@@ -2,10 +2,12 @@ import streamlit as st
 
 # embed streamlit docs in a streamlit app
 st.set_page_config(layout="centered")
-st.header("Wie geht es weiter?")
+st.header("Nachinformation für Teilnehmende")
 st.markdown(""" 
-            Wenn Sie Fragen oder Anregungen haben, schreiben Sie mir gerne unter brodela@cardiff.ac.uk  
+            <iframe src="https://drive.google.com/file/d/15yijN2-iZ2TPGFdiCgNtKyTiqSVdAsa-/preview" style="width: 100%; height: 1000px; max-height: 94vh" allow="autoplay"></iframe>  
+              
+            Wenn Sie Fragen und Anregungen haben, oder einen Austausch zur Teilnahme an der Studie und der dargestellten Thematik wünschen, schreiben Sie mir gerne unter brodela@cardiff.ac.uk  
             Vielen Dank für Ihre Zeit und das Engagement, welches ich sehr zu schätzen weiß.  
             Arne Brödel
-            """)
+            """,unsafe_allow_html=True)
 
