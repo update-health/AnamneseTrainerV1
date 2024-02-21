fc9bab8a6d3a200faa8e339bb423b89966123cff
Arnold85 <arne.broedel@gmail.com>
2024-02-21 20:24:58 +0000

    header

--- pages/8_Danke.py
+++ pages/8_Danke.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="centered")
 # Einbinden von benutzerdefinierten CSS-Stilen für die App
 with open("styles/styles.css") as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-st.header("Nachinformation für Teilnehmende")
+st.header("Abschließende Information für Teilnehmende")
 st.markdown(""" 
             <iframe src="https://drive.google.com/file/d/15yijN2-iZ2TPGFdiCgNtKyTiqSVdAsa-/preview" style="width: 100%; height: 1000px; max-height: 94vh" allow="autoplay"></iframe>  
               
